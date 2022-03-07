9f833e9143c14659498c60159758c8bb16062166
cameron <34195086+cameronos@users.noreply.github.com>
2022-03-07 01:35:14 -0500

    throw file

---
+++ health-check.sh
@@ -0,0 +1,58 @@
+commit=true
+origin=$(git remote get-url origin)
+if [[ $origin == *statsig-io/statuspage* ]]
+then
+  commit=false
+fi
+
+KEYSARRAY=()
+URLSARRAY=()
+
+urlsConfig="./urls.cfg"
+echo "Reading $urlsConfig"
+while read -r line
+do
+  echo "  $line"
+  IFS='=' read -ra TOKENS <<< "$line"
+  KEYSARRAY+=(${TOKENS[0]})
+  URLSARRAY+=(${TOKENS[1]})
+done < "$urlsConfig"
+
+echo "***********************"
+echo "Starting health checks with ${#KEYSARRAY[@]} configs:"
+
+mkdir -p logs
+
+for (( index=0; index < ${#KEYSARRAY[@]}; index++))
+do
+  key="${KEYSARRAY[index]}"
+  url="${URLSARRAY[index]}"
+  echo "  $key=$url"
+
+  for i in 1 2 3 4;
+  do
+    response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
+    if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 302 ] || [ "$response" -eq 307 ]; then
+      result="success"
+    else
+      result="failed"
+    fi
+    if [ "$result" = "success" ]; then
+      break
+    fi
+    sleep 5
+  done
+  dateTime=$(date +'%Y-%m-%d %H:%M')
+  if [[ $commit == true ]]
+  then
+    echo $dateTime, $result >> "logs/${key}_report.log"
+    echo "$(tail -2000 logs/${key}_report.log)" > "logs/${key}_report.log"
+  else
+    echo "    $dateTime, $result"
+  fi
+done
+
+if [[ $commit == true ]]
+then
+  git config --global user.name 'empty'
+fi
---
+++ incident_report.md
@@ -0,0 +1,14 @@
+<!--Use three dashes to make a bar, use h3s and margin-bots --->
+#### Mar 6, 2022
+<div>
+<h3 style="margin-bottom:5px">Main website availability</h3>
+Resolved (12:58 pm EST) - This incident has been resolved. For 18 hours, the website had a loss of availability.
+</div>
+
+---
+
+#### Mar 5, 2022
+<div>
+<h3 style="margin-bottom:5px">Main website availability</h3>
+Reported (6:58 pm EST) - Many users have reported a lack of accessibility to the website.
+</div>
